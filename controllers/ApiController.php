231655195787485750332b0734a2bfd16419508a
Mehdi Achour <machour@gmail.com>
2015-10-31 23:54:45 +0100

    Fix host

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -15,7 +15,7 @@ use Yii;
  * This is a sample server Petstore server.  You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, you can use the api key `special-key` to test the authorization filters.
  *
  * @basePath /v2
- * @host api.fly-car.local
+ * @host swagger.local
  * @scheme http
  *
  * @termsOfService http://swagger.io/terms/
